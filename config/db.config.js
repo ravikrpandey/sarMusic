8d91601394b72e3111b0529c5c003b77773c9895
shashipandey <kant72506@gmail.com>
2024-04-17 00:09:41 +0530

    config

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,30 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "sql@shashi",
+//   DB: "sarmusic",
+//   dialect: "mysql",
+//   logging: false,
+//   camelCase: true, 
+//   additional: {
+//       timestamps: false
+//   },
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// }
+
+console.log("Welcome")
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "sql@shashi",
-  DB: "sarmusic",
+  HOST: "sql10.freesqldatabase.com",
+  USER: "sql10698908",
+  PASSWORD: "ze1QSKnUew",
+  DB: "sql10698908",
   dialect: "mysql",
   logging: false,
   camelCase: true, 
@@ -17,6 +39,3 @@ module.exports = {
     idle: 10000
   }
 }
-
-console.log("Welcome")
-
